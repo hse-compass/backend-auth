5c1d64aefdaabce45503990cdbbecf0bffa25101
Daniil <thekevindit@gmail.com>
2025-02-04 18:29:48 +0300

    Переработка авторизации через register, добавление проверок

--- app/main.py
+++ app/main.py
@@ -14,12 +14,14 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://thekevindit.zapto.org", 
         "https://web.postman.co", 
         "http://thekevindit.zapto.org:3000",
+        "http://thekevindit.zapto.org:8055",
         "http://localhost:3000",
         "http://localhost",
         "http://127.0.0.1"
@@ -31,22 +33,3 @@ app.add_middleware(
 
 # Подключаем роутер, указываем префикс и тег (опционально)
 app.include_router(users.router, prefix="/api/v1/users", tags=["users"])
-
-@app.exception_handler(HTTPException)
-def custom_http_exception_handler(request, exc: HTTPException):
-    logger.error(f"HTTPException {exc.status_code}: {exc.detail}")
-
-    error_messages = {
-        400: "Некорректный запрос.",
-        401: "Неверный или просроченный токен.",
-        403: "У вас нет прав для доступа к этому ресурсу.",
-        404: "Запрашиваемый ресурс не найден.",
-        500: "Внутренняя ошибка сервера. Попробуйте позже."
-    }
-
-    message = error_messages.get(exc.status_code, "Неизвестная ошибка.")
-
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={"status": "error", "message": message}
-    )
--- app/routers/users.py
+++ app/routers/users.py
@@ -12,7 +12,7 @@ from ..database import SessionLocal, Base
 from ..models import User
 from ..schemas import UserCreate, UserLogin, TokenData
 from ..security import verify_password, get_password_hash, create_access_token, create_refresh_token
-from ..config import DIRECTUS_API_URL, DIRECTUS_ADMIN_TOKEN, ALGORITHM, REFRESH_SECRET_KEY,SECRET_KEY
+from ..config import DIRECTUS_API_URL, DIRECTUS_ADMIN_TOKEN, ALGORITHM, REFRESH_SECRET_KEY, SECRET_KEY
 from ..database import SessionLocal
 
 logger = logging.getLogger(__name__)
@@ -36,12 +36,10 @@ def register_user(user_data: UserCreate, db: Session = Depends(get_db)):
 
     # Регистрируем пользователя в Directus
     response = requests.post(
-        f"{DIRECTUS_API_URL}/users",
-        headers={"Authorization": f"Bearer {DIRECTUS_ADMIN_TOKEN}"},
+        f"{DIRECTUS_API_URL}/users/register",
         json={
             "email": user_data.email,
             "password": user_data.password,
-            "role": "55a9bff6-4e25-44c2-ba96-12be86f79d31"
         }
     )
 
@@ -49,14 +47,28 @@ def register_user(user_data: UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Неверный формат email или пароля.")
     elif response.status_code == 500:
         raise HTTPException(status_code=500, detail="Ошибка на стороне Directus.")
-    elif response.status_code != 200:
-        logger.error(f"Unexpected Directus response: {response.status_code}, {response.json()}")
+    elif response.status_code not in {200, 201, 204}:  # Обрабатываем возможные коды успеха
+        try:
+            error_details = response.json()
+        except requests.exceptions.JSONDecodeError:
+            error_details = "Invalid response from Directus"
+        logger.error(f"Unexpected Directus response: {response.status_code}, {error_details}")
         raise HTTPException(status_code=500, detail="Ошибка регистрации в Directus.")
 
+    response = requests.get(
+        f"{DIRECTUS_API_URL}/users?filter[email][_eq]={user_data.email}",
+        headers={"Authorization": f"Bearer {DIRECTUS_ADMIN_TOKEN}"},
+    )
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=500, detail="Ошибка получения данных пользователя из Directus.")
+
     directus_data = response.json()
-    id_directus = directus_data.get("data", {}).get("id")
-    if not id_directus:
+    users_list = directus_data.get("data", [])
+    if not users_list:
         raise HTTPException(status_code=500, detail="Ошибка получения ID Directus.")
+
+    id_directus = users_list[0].get("id")
 
     hashed_password = get_password_hash(user_data.password)
     new_user = User(
@@ -82,6 +94,24 @@ def login_user(user_data: UserLogin, db: Session = Depends(get_db)):
 
     if not verify_password(user_data.password, user.password_hash):
         raise HTTPException(status_code=400, detail="Неверный пароль.")
+
+    response = requests.get(
+        f"{DIRECTUS_API_URL}/users?filter[email][_eq]={user_data.email}",
+        headers={"Authorization": f"Bearer {DIRECTUS_ADMIN_TOKEN}"},
+    )
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=500, detail="Ошибка получения данных пользователя из Directus.")
+
+    directus_data = response.json()
+    users_list = directus_data.get("data", [])
+    if not users_list:
+        raise HTTPException(status_code=500, detail="Ошибка получения ID Directus.")
+
+    status = users_list[0].get("status")
+
+    if status == "unverified":
+        raise HTTPException(status_code=401, detail="Пользователь не подтвержден")
 
     user.last_login = datetime.utcnow()
     db.commit()
@@ -120,11 +150,13 @@ def refresh_token(request: Request):
 
     return {"access_token": new_access_token}
 
+
 @router.post("/logout")
 def logout():
     response = JSONResponse(content={"status": "ok"})
     response.delete_cookie("refresh_token")
     return response
+
 
 @router.get("/me")
 def get_me(request: Request, db: Session = Depends(get_db)):
@@ -153,6 +185,5 @@ def get_me(request: Request, db: Session = Depends(get_db)):
     return {
         "email": user.email,
         "id": user.id,
-        "last_login": user.last_login,
+        "directus_id": user.id_directus,
     }
-
